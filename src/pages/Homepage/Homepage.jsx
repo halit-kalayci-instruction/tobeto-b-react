cf78e10e9ad87275af03f434e9fba97fa735f732
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-25 13:34:39 +0300

    href vs link

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Homepage() {
-	return <div>Homepage</div>;
+	return (
+		<div>
+			Homepage
+			<br />
+			{/* SPA'larda href attribute'ı kullanımı yanlıştır. */}
+			<a href="/products">Ürünler Sayfası</a>
+			<br />
+			<Link to={"/products"}>Ürünler Sayfası</Link>
+		</div>
+	);
 }
