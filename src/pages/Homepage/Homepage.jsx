94f488f33ba91b9c822cd9dab6a85b7fb318f9c6
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-25 13:57:56 +0300

    async-await-promise

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Link} from "react-router-dom";
 
 export default function Homepage() {
+	const myAsyncFunction = () => {
+		return new Promise((resolve, reject) => {
+			// async işlem
+			setTimeout(() => {
+				//resolve("veri örneği"); // işlemin başarılı olması
+				reject("istek başarısız"); // işlemin başarısız olması
+			}, 3000);
+		});
+	};
+
+	useEffect(() => {
+		// fire and forget
+		//thenCatchFinally();
+		asyncAwait();
+	}, []);
+
+	// ES2017 => async, await
+
+	// Vanilla Java, Vanilla C#, Vanilla Javascript
+
+	const asyncAwait = async () => {
+		try {
+			let response = await myAsyncFunction();
+			console.log(response);
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	const thenCatchFinally = () => {
+		console.log("loading başladı");
+		myAsyncFunction()
+			.then(response => {
+				console.log("Bir cevap geldi:", response);
+			})
+			.catch(err => {
+				console.log("Bir hata geldi:", err);
+			})
+			.finally(() => {
+				console.log("Async işlem sonlandı.");
+				console.log("loading bitti");
+			});
+	};
+	// JS ASYNC
+	// Built-in Promise
 	return (
 		<div>
 			Homepage
