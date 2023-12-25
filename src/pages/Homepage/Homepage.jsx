773fee9c3aeec877a258d8a9b7336cfa7770f3ce
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-25 14:23:55 +0300

    .

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -2,51 +2,15 @@ import React, {useEffect} from "react";
 import {Link} from "react-router-dom";
 
 export default function Homepage() {
-	const myAsyncFunction = () => {
-		return new Promise((resolve, reject) => {
-			// async işlem
-			setTimeout(() => {
-				//resolve("veri örneği"); // işlemin başarılı olması
-				reject("istek başarısız"); // işlemin başarısız olması
-			}, 3000);
-		});
-	};
+	// http isteği
 
 	useEffect(() => {
-		// fire and forget
-		//thenCatchFinally();
-		asyncAwait();
+		// gelen veriyi json olarak console'a yazdır.
+		fetch("https://dummyjson.com/products")
+			.then(response => response.json())
+			.catch(err => console.log(err));
 	}, []);
 
-	// ES2017 => async, await
-
-	// Vanilla Java, Vanilla C#, Vanilla Javascript
-
-	const asyncAwait = async () => {
-		try {
-			let response = await myAsyncFunction();
-			console.log(response);
-		} catch (e) {
-			console.log(e);
-		}
-	};
-
-	const thenCatchFinally = () => {
-		console.log("loading başladı");
-		myAsyncFunction()
-			.then(response => {
-				console.log("Bir cevap geldi:", response);
-			})
-			.catch(err => {
-				console.log("Bir hata geldi:", err);
-			})
-			.finally(() => {
-				console.log("Async işlem sonlandı.");
-				console.log("loading bitti");
-			});
-	};
-	// JS ASYNC
-	// Built-in Promise
 	return (
 		<div>
 			Homepage
