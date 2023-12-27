6122be711b03c4467a5769df30242b5083ccf314
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-27 14:22:01 +0300

    remove unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import {ReactElement} from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App(): ReactElement {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
 				<p>
 					Edit <code>src/App.tsx</code> and save to reload.
 				</p>
