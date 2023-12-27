ed391129bb6b16ccad49a07e2560de425df3a73d
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-27 15:01:47 +0300

    .

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,10 +1,13 @@
 import axios from "axios";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {GetAllProductsModel} from "../../models/responses/GetAllProductsModel";
+import {ProductModel} from "../../models/responses/ProductModel";
 
 type Props = {};
 
 const Homepage = (props: Props) => {
+	const [products, setProducts] = useState<ProductModel[]>([]);
+
 	useEffect(() => {
 		fetchProducts();
 	}, []);
@@ -14,14 +17,16 @@ const Homepage = (props: Props) => {
 		axios
 			.get<GetAllProductsModel>("https://dummyjson.com/products")
 			.then(response => {
-				console.log(response.data);
+				setProducts(response.data.products);
 			});
 	};
 
 	return (
 		<div className="container">
 			<div className="row">
-				<div className="col-3"></div>
+				{products.map(product => (
+					<div key={product.id} className="col-3"></div>
+				))}
 			</div>
 		</div>
 	);
